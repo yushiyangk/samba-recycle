c207a96f556cdb0a8f89ed8e9a3ceaeca6c4d141
Yu Shiyang <yu.shiyang@gnayihs.uy>
2022-09-04 03:33:58 +0800

    Updated to have configurable threshold (default 28)

--- samba-recycle.sh
+++ samba-recycle.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-# Search .recycle subdirectory of samba shares and remove files older than 14 days
-#  then remove all empty subdirectories
+# Search .recycle subdirectory of samba shares and remove files older than
+#  RECYCLE_AGE number of days, then remove all empty subdirectories
+# RECYCLE_AGE is 28 by default if no argument is provided
 
 # Yu Shiyang <yu.shiyang@gnayihs.uy>
 
+RECYCLE_AGE=28
+[ "$1" ] && {
+	RECYCLE_AGE="$1"
+}
+
 grep -E '^path\s*=\s*.+' /etc/samba/smb.conf | sed 's/^path\s*=\s*//' | while read line; do
 	if [ -d "$line/.recycle" ]; then
-		find "$line/.recycle" -mindepth 1 -mtime +14 -delete
+		find "$line/.recycle" -mindepth 1 -mtime +"$RECYCLE_AGE" -delete
 		find "$line/.recycle" -mindepth 1 -type d -empty -delete
 	fi
 done
